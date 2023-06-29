3fc88a5699ac22023e60d6aaae0c957c66e99758
sumicet <mihailescuc@outlook.com>
2023-06-29 21:15:54 +0300

    remove unused svg

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,6 +1,9 @@
 import { chakra } from '@chakra-ui/react';
 import NextImage from 'next/image';
 
+/**
+ * A `chakra-ui` `Image` component that extends `next.js`'s `Image`.
+ */
 export const Image = chakra(NextImage, {
     shouldForwardProp: prop =>
         ['width', 'height', 'src', 'alt', 'placeholder', 'blurDataURL', 'loader'].includes(prop),
--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,6 +1,9 @@
 import NextLink, { LinkProps as NextLinkProps } from 'next/link';
 import { Link as ChakraLink, LinkProps } from '@chakra-ui/react';
 
+/**
+ * A `chakra-ui` `Link` component that extends `next.js`'s `Link`.
+ */
 export function Link(props: LinkProps & NextLinkProps) {
     return <ChakraLink {...props} as={NextLink} />;
 }
