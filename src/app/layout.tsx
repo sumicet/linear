ddfe7821081c10d4aa0d339e4d768303f3194d45
sumicet <mihailescuc@outlook.com>
2023-06-30 17:32:21 +0300

    add missing slash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
     icons: [
         {
             rel: 'icon',
-            url: 'favicon.ico',
+            url: '/favicon.ico',
         },
     ],
     twitter: {
